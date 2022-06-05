0f78bc3593f0064566b4ff111fea8e3867a6839c
Faizura Zadri <faizurazadri08@gmail.com>
2022-06-05 12:34:38 +0700

    Initial commit

---
+++ app/src/main/java/com/urangcoding/shop/user/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.urangcoding.shop.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.urangcoding.shop.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
